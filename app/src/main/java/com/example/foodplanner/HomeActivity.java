9d8ceb470c102230d3bb28faf839313c5b873d17
saraeltlt <47868870+saraeltlt@users.noreply.github.com>
2023-02-03 02:34:18 +0200

    "bottombar & toolbar"

--- app/src/main/java/com/example/foodplanner/HomeActivity.java
+++ app/src/main/java/com/example/foodplanner/HomeActivity.java
@@ -1,17 +1,20 @@
 package com.example.foodplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-     // Intent intent = new Intent( MainActivity.this,HomeActivity.class);
-      //startActivity(intent);
+        setContentView(R.layout.activity_home);
+
+
+
     }
 }
