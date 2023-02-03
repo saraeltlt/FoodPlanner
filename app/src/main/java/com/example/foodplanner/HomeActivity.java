42410208809894bda1d1468f7cd56708b8637ccf
saraeltlt <47868870+saraeltlt@users.noreply.github.com>
2023-02-03 13:56:21 +0200

    "HOME navigations"

--- app/src/main/java/com/example/foodplanner/HomeActivity.java
+++ app/src/main/java/com/example/foodplanner/HomeActivity.java
@@ -2,9 +2,16 @@ package com.example.foodplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -13,6 +20,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        NavController navController = Navigation.findNavController(this,R.id.navHostFragment);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+
 
 
 
